7ce1480418f01bc9cac2b23a37f8c90bb4879f4c
itsmerims <johnrimuelbigata@gmail.com>
2023-01-21 19:27:25 +0800

    new file:   .gitignore 	new file:   earth.jpg 	new file:   earthnormal.jpg 	new file:   index.html 	new file:   javascript.svg 	new file:   logo.png 	new file:   main.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.svg 	new file:   rims.jpg 	new file:   space.jpg 	new file:   space.png 	new file:   spinme.png 	new file:   spinmeright.png 	new file:   star.png 	new file:   style.css 	new file:   tailwind.config.cjs 	new file:   venus.png 	new file:   venustexture.png 	new file:   widestar.png

---
+++ main.js
@@ -0,0 +1,89 @@
+import './style.css'
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.1, 100);
+camera.position.z = 20
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(2);
+renderer.setSize(size.width, size.height);
+
+renderer.render(scene, camera)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+
+const light = new THREE.PointLight(0xffffff);
+const ambient = new THREE.AmbientLight(0x050505)
+light.position.set(0, 10, 10);
+scene.add(light, ambient)
+
+// Background
+
+// const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+// scene.background = spaceTexture;
+
+// earth
+const earthTexture = new THREE.TextureLoader().load('earth.jpg');
+const earthnormal = new THREE.TextureLoader().load('earthnormal.jpg')
+
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 64, 64),
+  new THREE.MeshStandardMaterial({
+    map: earthTexture,
+    normalMap: earthnormal
+  }),
+)
+scene.add(earth)
+
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.05, 24, 24);
+//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//   const star = new THREE.Mesh(geometry, material);
+
+//   const [x, y, z] = Array(100)
+//     .fill()
+//     .map(() => THREE.MathUtils.randFloatSpread(100));
+
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
+
+// Array(2500).fill().forEach(addStar);
+
+
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth
+  size.height = window.innerHeight
+
+  camera.aspect = size.width / size.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(size.width, size.height)
+})
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  earth.rotation.y += 0.002;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
